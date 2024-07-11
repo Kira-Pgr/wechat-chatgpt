cc1833911f12e79651fe6674fa437c6cb63bcd16
Kira-Pgr <aieternal@outlook.com>
2024-07-11 15:01:41 +0800

    update

--- bot.ts
+++ bot.ts
@@ -1,6 +1,7 @@
-import { WechatyBuilder } from '@juzi/wechaty'
+import { WechatyBuilder, types, ScanStatus } from '@juzi/wechaty'
 import * as Sentry from '@sentry/node'
 import { MemoryCard } from 'memory-card'
+import inquirer from 'inquirer'
 
 import * as message from './event/message'
 import * as friendShip from './event/friend-ship'
@@ -62,6 +63,30 @@ if (require.main === module) {
         .catch(e => {
           Sentry.captureException(e)
         })
+    })
+    .on('verify-code', async (id: string, message: string, scene: types.VerifyCodeScene, status: types.VerifyCodeStatus) => {
+        // 需要注意的是，验证码事件不是完全即时的，可能有最多10秒的延迟。
+        // 这与底层轮询二维码状态的时间间隔有关。
+        if (status === types.VerifyCodeStatus.WAITING && scene === types.VerifyCodeScene.LOGIN && id === store.qrcodeKey) {
+          console.log(`receive verify-code event, id: ${id}, message: ${message}, scene: ${types.VerifyCodeScene[scene]} status: ${types.VerifyCodeStatus[status]}`)
+          const { verifyCode } = await inquirer.prompt([
+            {
+              type: 'input',
+              name: 'verifyCode',
+              message: 'Please enter the verification code:',
+            },
+          ])
+          try {
+            await bot.enterVerifyCode(id, verifyCode) // 如果没抛错，则说明输入成功，会推送登录事件
+            return
+          } catch (e) {
+            console.log((e as Error).message)
+            // 如果抛错，请根据 message 处理，目前发现可以输错3次，超过3次错误需要重新扫码。
+            // 错误关键词: 验证码错误输入错误，请重新输入
+            // 错误关键词：验证码错误次数超过阈值，请重新扫码'
+            // 目前不会推送 EXPIRED 事件，需要根据错误内容判断
+          }
+        }
     })
     .on('login', async (user) => {
       const name = user.name()
